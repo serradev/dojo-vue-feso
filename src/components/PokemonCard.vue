375e66326e35f4f472fe5992a9a046187698aa4b
Fernanda do Canto Nogueira <fernanda.canto.ngr@gmail.com>
2019-07-25 19:50:46 -0300

    Inclusão dos componentes PokemonCard e PokemonList

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,14 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'PokemonCard',
+  props: {
+    id: String
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/PokemonList.vue
@@ -0,0 +1,14 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'PokemonList',
+  props: {
+    id: String
+  }
+}
+</script>
+
+<style>
+</style>
