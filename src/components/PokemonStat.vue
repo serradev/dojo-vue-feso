507167bc65f01fecfd37470deb80c70741889003
Fernanda do Canto Nogueira <fernanda.canto.ngr@gmail.com>
2019-07-25 21:32:53 -0300

    Pokemon Stat e ajustes.

--- src/components/PokemonStat.vue
+++ src/components/PokemonStat.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <h5 class="card-title">Stats</h5>
+
+    <div class="list-group">
+      <button type="button" class="list-group-item list-group-item-action">Cras justo odio</button>
+      <button type="button" class="list-group-item list-group-item-action">Dapibus ac facilisis in</button>
+      <button type="button" class="list-group-item list-group-item-action">Morbi leo risus</button>
+      <button type="button" class="list-group-item list-group-item-action">Porta ac consectetur ac</button>
+      <button type="button" class="list-group-item list-group-item-action" disabled>Vestibulum at eros</button>
+    </div>
   </div>
 </template>
 
