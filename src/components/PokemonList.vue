74b0b7ecde3857d6f59e03ffa9b3be9680dc2a28
Bernardo Esbérard <bernardbr@gmail.com>
2019-07-25 21:20:38 -0300

    Obtém a lista inicial de pokemons

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,24 +1,59 @@
 <template>
-
   <div class="list-group">
-    <button type="button" class="list-group-item list-group-item-action ">
-      Cras justo odio
-    </button>
+    <button type="button" class="list-group-item list-group-item-action">Cras justo odio</button>
     <button type="button" class="list-group-item list-group-item-action">Dapibus ac facilisis in</button>
     <button type="button" class="list-group-item list-group-item-action">Morbi leo risus</button>
     <button type="button" class="list-group-item list-group-item-action">Porta ac consectetur ac</button>
     <button type="button" class="list-group-item list-group-item-action" disabled>Vestibulum at eros</button>
-  </div>
 
+    <ul id="example-1">
+      <li v-for="p in pokemons" :key="p.order">
+        {{ p.name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'PokemonList',
+  name: 'pokemonList',
   props: {
     titulo: String
+  },
+  data() {
+    this.pokemons = [];
+  },  
+  methods: {
+    obterListaPokemon(offset) {
+      const URL = 'https://pokeapi.co/api/v2/pokemon/';
+      axios
+        .get(URL, {
+          params: {
+            offset,
+            limit: 20
+          }
+        })
+        .then(response => {
+          let data = response.data.results;
+        
+
+          data.forEach(item => {
+            let pokemon = {
+              name: item.name,
+              order: parseInt(item.url.match(/[^/]+(?=\/$)/g)[0])
+            };
+
+            this.pokemons.push(pokemon);
+          });
+        })
+    }
+  },
+  mounted() {
+    this.obterListaPokemon(0);
   }
-}
+};
 </script>
 
 <style>
