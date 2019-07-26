e150e31817a31c76005aec969dc0149bf5010478
Bernardo Esbérard <bernardbr@gmail.com>
2019-07-25 21:59:03 -0300

    Adiciona o evento de seleção do Pokemon

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="list-group">
-    <button  v-for="p in pokemons" :key="p.order" type="button" class="list-group-item list-group-item-action">
-      {{p.name}}
+    <button  v-for="p in pokemons" :key="p.order" type="button" class="list-group-item list-group-item-action" v-on:click="obterDadosPokemon(p)">
+      {{p.order}} - {{p.name}}
     </button>
+
+    {{pokemonSelecionado}}
   </div>
 </template>
 
@@ -16,35 +18,62 @@ export default {
   },
   data: function() {
     return {
-      pokemons: []
+      pokemons: [],
+      pokemonSelecionado: null
     }    
   },  
   methods: {
-    obterListaPokemon(offset) {
+    obterDadosPokemon(pokemon) {
+      return axios
+        .get(pokemon.url)
+        .then(response => {
+          let data = response.data;
+          let pokemonStats = [];
+          data.stats.forEach(stat => {
+            pokemonStats.push({
+              baseStat: stat.base_stat,
+              name: stat.stat.name,
+              effort: stat.effort
+            });
+          });
+
+          this.pokemonSelecionado = {
+            name: data.name,
+            order: data.order,
+            stats: pokemonStats,
+            image: data.sprites.front_default,
+            type: data.types[0].type.name
+          };
+        });
+    },
+    carregarPokemons(pagina) {
       const URL = 'https://pokeapi.co/api/v2/pokemon/';
       return axios
         .get(URL, {
           params: {
-            offset,
+            offset: pagina,
             limit: 20
           }
         })
         .then(response => {
           let data = response.data.results;
-          let retorno = [];
           data.forEach(item => {
             let pokemon = {
               name: item.name,
-              order: parseInt(item.url.match(/[^/]+(?=\/$)/g)[0])
+              order: parseInt(item.url.match(/[^/]+(?=\/$)/g)[0]),
+              url: item.url
             };
 
             this.pokemons.push(pokemon);
           });
+
+          
+          return this.pokemons;
         })
     }
   },
   mounted() {
-    this.obterListaPokemon(0);
+    this.carregarPokemons(0);
   }
 };
 </script>
